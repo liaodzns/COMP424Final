b8911c96bd10cc6be93d43624bf502fcf3bf6672
Alistair Begg <alistairvbegg@gmail.com>
2025-11-08 17:07:44 -0500

    Forgot to commit init.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -3,3 +3,4 @@ from .human_agent import HumanAgent
 from .student_agent import StudentAgent
 from .greedy_corners_agent import StudentAgent
 from .heuristic_agent import HeuristicAgent
+from .heuristic_agent_basic import HeuristicAgentBasic
